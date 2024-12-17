919e1fd45c5d70502bb78e9d434959e5902e4762
luismarymo <luchita0420@gmail.com>
2024-12-17 16:49:09 +0100

    Fixed spotlight functionality and added transition on hover

--- src/js/Spotlight.mjs
+++ src/js/Spotlight.mjs
@@ -4,18 +4,17 @@ export default class Spotlight {
     }
 
     async init() {
-        const options = await this.dataSource.getData(this);
+        const cat = await this.dataSource.getData(this);
 
-        this.displaySpotlight(options);
+        this.displaySpotlight(cat);
     }
 
-    displaySpotlight(list) {
+    displaySpotlight(cat) {
         const spotlightContainer = document.querySelector(".spotlight");
 
-        let random = list[Math.floor(Math.random() * list.length)];
-
         spotlightContainer.innerHTML = `
-        <img src="${random.url}" alt="Image of ${random.breeds[0].name}">
-        <h2 class="card__name">${random.breeds[0].name}</h2>`
+        <img src="${cat[0].url}" alt="Image of ${cat[0].breeds[0].name}">
+        <h2>${cat[0].breeds[0].name}</h2>
+        <p>${cat[0].breeds[0].description}</p>`
     }
 }
