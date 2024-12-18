4bf120b7db7b6947ac411d769682d249fd78b55b
luismarymo <luchita0420@gmail.com>
2024-12-18 09:10:44 +0100

    Carousel in home page

--- src/js/Spotlight.mjs
+++ src/js/Spotlight.mjs
@@ -1,9 +1,30 @@
+let imgIndex = 0;
+
+function displayCarousel() {
+    let images = document.querySelectorAll(".carousel-img");
+
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = "none";
+    }
+
+    imgIndex++;
+
+    if (imgIndex > images.length) {
+        imgIndex = 1;
+    }
+
+    images[imgIndex-1].style.display = "block";
+    setTimeout(displayCarousel, 2500);
+}
+
 export default class Spotlight {
     constructor(dataSource) {
         this.dataSource = dataSource;
     }
 
     async init() {
+        displayCarousel();
+
         const cat = await this.dataSource.getData(this);
 
         this.displaySpotlight(cat);
